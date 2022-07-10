21926128741e93f03e35c5c4e62e4abbe8050bc9
Adnan Supic <adnan@tappable.co.uk>
2022-07-10 17:46:33 +0200

    finish tree building

--- tree-building/tree_building.go
+++ tree-building/tree_building.go
@@ -1,5 +1,10 @@
 package tree
 
+import (
+	"errors"
+	"sort"
+)
+
 type Record struct {
 	ID     int
 	Parent int
@@ -13,8 +18,36 @@ type Node struct {
 }
 
 func Build(records []Record) (*Node, error) {
-	depth := 0
-	for r := range records {
-
+	length := len(records)
+	if length == 0 {
+		return nil, nil
 	}
+
+	sort.Slice(records, func(i, j int) bool {
+		return records[i].ID < records[j].ID
+	})
+
+	var nodes = make([]*Node, length)
+	for _, r := range records {
+		if r.ID < 0 || r.ID >= length {
+			return nil, errors.New("id out of bounds")
+		}
+		if r.Parent == r.ID && r.ID != 0 {
+			return nil, errors.New("r.id > parent id")
+		}
+		if r.Parent > r.ID {
+			return nil, errors.New("root has parent")
+		}
+		if nodes[r.ID] != nil {
+			return nil, errors.New("r.id already inserted")
+		}
+		nodes[r.ID] = &Node{ID: r.ID}
+		if r.ID == 0 {
+			continue
+		}
+		parent := nodes[r.Parent]
+		parent.Children = append(parent.Children, nodes[r.ID])
+	}
+
+	return nodes[0], nil
 }
